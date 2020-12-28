1de66f4c05a85727ddd878e4ff36b43fa734dbc7
maxyodedara5 <maxyodedara5@gmail.com>
2020-12-28 23:10:47 +0530

    Lists and dictionaries
    Added code for ( " , { } [ ] ) for lists and dictionaries

--- Main.cpp
+++ Main.cpp
@@ -1,41 +1,23 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <stack>
 
 
-void AddToJSON(std::string toAdd , std::string jsonString , bool& isKey) {
-    
-    if (isKey == true) {
-        jsonString += toAdd; 
-        jsonString += ":"; 
-        isKey = false;
-        return;
-    }
-    else
-    {
-        jsonString += toAdd;
-        jsonString += ",";
-        isKey = true;
-        return;
-    }
-}
+std::string InputFile() {
+    std::cout << "Enter the file name :" << std::endl;
+    std::string fileName = "bbb_sunflower.torrent";
+    //std::cin >> fileName; 
+    //std::cout << fileName << std::endl;
 
-
-int main() {
-
-	std::cout << "Enter the file name :" << std::endl;
-	std::string fileName = "sample.torrent"; 
-	//std::cin >> fileName; 
-    std::cout << fileName << std::endl;
-   
     std::ifstream inf{ fileName };
-    
+
     // If we couldn't open the output file stream for reading
     if (!inf)
     {
         // Print an error and exit
         std::cerr << "Error Input file cannot be read" << std::endl;
-        return 1;
+        return "Error\n";
     }
 
     // While there's still stuff left to read
@@ -43,16 +25,110 @@ int main() {
     while (inf)
     {
         // read stuff from the file into a string and print it
-        
+
         std::getline(inf, strInput);
-        std::cout << strInput << '\n';
+        //std::cout << strInput << '\n';
+    }
+    return strInput;
+}
+
+std::string OutputFile(std::string jsonInput) {
+    // Write to created JSON file 
+
+    // ofstream is used for writing files
+
+    std::string outputFilename;
+
+    std::ofstream outf{ "Torrent.json" };
+
+    // If we couldn't open the output file stream for writing
+    if (!outf)
+    {
+        // Print an error and exit
+        std::cerr << "Error in writing file" << std::endl;
+        return "Error";
     }
 
+    // We'll write two lines into this file
+    outf << jsonInput << '\n';
+    return "Success";
+}
+
+//function to add text to json string
+/*
+string toAdd        - is the word to be appended to jsonString
+string jsonString   - is current json file created
+bool& isKey         - is to determine if the word is key or value, if key then : needs to be appended 
+                      after quotes and if its value then , needs to be appended
+*/
+void AddToJSON(std::string toAdd , std::string& jsonString , bool& isKey , bool arrayStart) {
+    
+    // if its inside array only "," needs to added 
+    if (arrayStart == true) {
+        jsonString += toAdd;
+        jsonString += "\"";
+        jsonString += ",";
+        isKey = true;
+        return;
+    }
+    else
+    {
+        //inside json key value pairs so "," and ":" both need to be managed 
+        if (isKey == true) {
+            jsonString += toAdd;
+            jsonString += "\"";
+            jsonString += ":";
+
+            isKey = false;
+            return;
+        }
+        else
+        {
+            jsonString += toAdd;
+            jsonString += "\"";
+            jsonString += ",";
+            isKey = true;
+            return;
+        }
+    }
+
+   
+}
+
+//function to add text(number) to json string
+/*
+* 
+*  TODO - Negative numbers 
+* 
+string toAdd        - is the word to be appended to jsonString
+string jsonString   - is current json file created
+bool& isKey         - is to determine if the word is key or value, if key then : needs to be appended
+                      after quotes and if its value then , needs to be appended
+bool number         - to confirm if word is number or word , if its number then quotes wont be needed 
+*/
+void AddToJSON(std::string toAdd, std::string& jsonString, bool& isKey ) {
+
+   
+        jsonString += toAdd;
+        jsonString += ",";
+        isKey = true;
+        return;
+    
+}
+
+int main() {
+
+	
+
+    std::string strInput = InputFile();
 
     int inputLen = strInput.length();
     std::string jsonInput; 
     int current = 0; 
     std::string number; 
+    bool iskey = true;
+    std::stack<std::string> containerTrack;
+    bool arrayStart = false; 
     while (current != inputLen)
     {
 
@@ -66,14 +142,18 @@ int main() {
                 int numberOfLetters = stoi(number);
                 std::string word = strInput.substr(current + 1, numberOfLetters);
                 jsonInput += "\""; 
-                jsonInput += word;
-                jsonInput += "\"";
+                
+                AddToJSON(word, jsonInput, iskey , arrayStart);
+                //jsonInput += word;
+               
                 current += numberOfLetters + 1; 
             }
                
             continue;
         }
 
+        //bencode conversion for numbers 
+        // todo negative numbers 
         if (strInput[current] == 'i') {
             current++; //skipping i 
             number = ""; 
@@ -82,7 +162,8 @@ int main() {
                 current++;
             }
             
-            jsonInput += number; 
+            AddToJSON(number, jsonInput, iskey );
+            //jsonInput += number; 
 
             current++; //skipping e 
             continue;
@@ -92,11 +173,40 @@ int main() {
 
         if (strInput[current] == 'd') {
             jsonInput += "{";
+            containerTrack.push("{");
             current++;
             continue;
         }
+
+        if (strInput[current] == 'l') {
+            containerTrack.push("[");
+            arrayStart = true;
+            jsonInput += "[";
+            current++;
+            continue;
+        }
+
         if (strInput[current] == 'e') {
-            jsonInput += "}";
+            if (containerTrack.top() == "[") {
+                if (*(jsonInput.end()-1) == ',') {
+                    jsonInput.pop_back();
+                }
+                jsonInput += "]";
+                arrayStart = false; 
+                containerTrack.pop();
+            }
+            else
+            {
+                if (containerTrack.top() == "{") {
+                    if (*(jsonInput.end() - 1) == ',') {
+                        jsonInput.pop_back();
+                    }
+                    jsonInput += "}";
+                    arrayStart = false;
+                    containerTrack.pop();
+                }
+            }
+            
             current++;
             continue;
         }
@@ -105,24 +215,7 @@ int main() {
         current++;
     }
 
-  
-    // Write to created JSON file 
-
-    // ofstream is used for writing files
-
-    std::string outputFilename;
+    std::string outfileStatus = OutputFile(jsonInput);
     
-    std::ofstream outf{ "Torrent.json" };
-
-    // If we couldn't open the output file stream for writing
-    if (!outf)
-    {
-        // Print an error and exit
-        std::cerr << "Error in writing file" << std::endl;
-        return 1;
-    }
-
-    // We'll write two lines into this file
-    outf << jsonInput << '\n';
 	return 0;
 }
